2265e7ced9dbca0039d73480d6d06d7b2c35c2f2
Robert Hess <robert.hess@marquette.edu>
2023-12-08 19:37:44 -0600

    update

--- INSYFinalPhaseII/Customer.cs
+++ INSYFinalPhaseII/Customer.cs
@@ -9,7 +9,7 @@ namespace INSYFinalPhaseII
     public class Customer
     {
         private static int autoIncreament;
-        public int Id { get; set; }
+        public string Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
         public string FirstName { get; set; }
