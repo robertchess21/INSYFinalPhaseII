1309c171b3714126c95220186c662f3d3df71583
Nicholas Dembinsky <nicholas.dembinsky@marquette.edu>
2023-12-08 18:44:31 -0600

    created Appointment class

---
+++ INSYFinalPhaseII/Appointment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassProject.Model
+{
+    public class Appointment
+    {
+        private static int autoIncreament;
+        public int Id { get; set; }
+        public DateTime date { get; set; }
+
+        public string AdvisorType { get; set; }
+        //these specify the Id, date, and advisor type for the meeting 
+
+        public Appointment()
+        {
+            autoIncreament++;
+            Id = autoIncreament;
+        }
+    }
+}
