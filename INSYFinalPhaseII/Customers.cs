b2d90f8aec2204d303d86c81e3e2325f332c57d6
Robert Brown <r.brown@marquette.edu>
2023-12-12 19:37:28 -0600

    Created Customers class which will authenticate the username and password

--- INSYFinalPhaseII/Customers.cs
+++ INSYFinalPhaseII/Customers.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace INSYFinalPhaseII
 {
-    internal class Customers
+    public class Customers
     {
+        public List<Customer> customers { get; set; }
+
+        public Customers()
+        {
+            customers = new List<Customer>();
+        }
+
+        public Customer Authenticate(string username, string password)
+        {
+            var c = customers.Where(o => (o.Username == username) && (o.Password == password));
+
+            if (c.Count() > 0)
+            {
+                return c.First();
+            }
+            else
+            {
+                return null;
+            }
+        }
     }
 }
